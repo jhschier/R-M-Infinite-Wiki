3c2e4bad4a44f64fd87eabfbf0db441695a4a440
jhschier <jhschier30@hotmail.com>
2023-11-21 16:58:57 +0000

    finalizing cards styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/filters";
+import Cards from "./components/Cards/cards";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+
+  let [fetchedData, setFetchedData] = useState([]);
+
+  let { info, results } = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((response) => response.json());
+
+      setFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center creepster my-4">
+        <span className="text-success">Rick and Morty's </span>
+        <span className="text-primary"> Infinite WiKi</span>
+      </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards results={results} />
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Cards/cards.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styles from "./cards.module.scss";
+
+const cards = ({ results }) => {
+  let display;
+  console.log(results);
+  if (results) {
+    display = results.map((x) => {
+      let { id, name, image, gender, species, origin, location, status } = x;
+      return (
+        <div
+          key={id}
+          className={`${styles.border} col-4 position-relative mb-4`}
+        >
+          <div className="">
+            <img
+              src={image}
+              alt="characters"
+              className={`${styles.imgFluid} img-fluid`}
+            />
+            <div className="content">
+              <div className="fs-4 fw-bold mb-2">{name}</div>
+              <div className="mb-1">{gender}</div>
+              <div className="mb-1">{species}</div>
+              <div className="fs-4 fw-bold mb-1">Origin:</div>
+              <div className="">{origin.name}</div>
+              <div className="fs-4 fw-bold mb-1">Last Location:</div>
+              <div className="fs-6 fw-bold">{location.name}</div>
+            </div>
+          </div>
+          {(() => {
+            if (status === "Dead") {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-danger`}
+                >
+                  {status}
+                </div>
+              );
+            } else if (status === "Alive") {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-success`}
+                >
+                  {status}
+                </div>
+              );
+            } else {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-secondary`}
+                >
+                  {status}
+                </div>
+              );
+            }
+          })()}
+        </div>
+      );
+    });
+  } else {
+    display =
+      "Hey Morty, *BURRRRP* There's no character in this ANIMATED SERIES with this name! *BURRRRP*";
+  }
+
+  return <>{display}</>;
+};
+
+export default cards;
